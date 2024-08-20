ddf882cb2e5509b679152538ec2e2e65428d679d
Nazim <yasarnazimsari@gmail.com>
2024-08-20 20:30:53 +0300

    created homecoversection component

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,14 @@
+import { compareDesc } from "date-fns";
+import { parseISO } from "date-fns/fp";
+
 export const cx = (...className) => {
   return className.filter(Boolean).join(" ");
+};
+
+export const sortBlogs = (blogs) => {
+  return blogs
+    .slice()
+    .sort((a, b) =>
+      compareDesc(parseISO(a.publishedAt), parseISO(b.publishedAt))
+    );
 };
