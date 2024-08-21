e1d40a3754a35524edf00763346be9d18e6f16a3
Nazim <yasarnazimsari@gmail.com>
2024-08-21 22:03:41 +0300

    created redermdx and blogdetails component

--- src/app/blogs/[slug]/page.js
+++ src/app/blogs/[slug]/page.js
@@ -1,3 +1,5 @@
+import BlogDetails from "@/src/components/Blog/BlogDetails";
+import RenderMdx from "@/src/components/Blog/RenderMdx";
 import Tag from "@/src/components/Elements/Tag";
 import { allBlogs } from "contentlayer/generated";
 import Image from "next/image";
@@ -27,6 +29,11 @@ export default function BlogPage({ params }) {
           height={blog.image.height}
           className="aspect-square w-full h-full object-cover object-center"
         />
+      </div>
+      <BlogDetails blog={blog} slug={params.slug} />
+      <div className="grid grid-cols-12 gap-16 mt-8 px-10">
+        <div className="col-span-4">Toc</div>
+        <RenderMdx blog={blog} />
       </div>
     </article>
   );
