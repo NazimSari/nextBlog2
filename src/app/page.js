823b8ea8e7af228899f07e9839fdc2766c7288c0
Nazim <yasarnazimsari@gmail.com>
2024-08-21 14:13:05 +0300

    created recentpost component

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import { allBlogs } from "contentlayer/generated";
 import HomeCoverSection from "../components/Home/HomeCoverSection";
 import FeaturedPost from "../components/Home/FeaturedPost";
+import RecentPost from "../components/Home/RecentPost";
 
 export default function Home() {
   console.log(allBlogs);
@@ -8,6 +9,7 @@ export default function Home() {
     <main className="flex flex-col items-center justify-center">
       <HomeCoverSection blogs={allBlogs} />
       <FeaturedPost blogs={allBlogs} />
+      <RecentPost blogs={allBlogs} />
     </main>
   );
 }
